521db6d19725a05adb9960933eb91ad6e89e1aac
Lorenzo Battistela <lorenzowottrich@gmail.com>
2021-10-14 13:05:46 -0300

    deploy commit

---
+++ app.py
@@ -0,0 +1,38 @@
+import tweepy
+import schedule
+import time
+import os
+
+
+consumer_key = os.getenv('CONSUMER_KEY')
+consumer_secret = os.getenv('CONSUMER_SECRET')
+acess_token = os.getenv('ACCESS_TOKEN')
+acess_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
+
+def main():
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(acess_token, acess_token_secret)
+    api = tweepy.API(auth)
+    schedule.every(10).minutes.do(retweet(api))
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+def get_mentions(api):
+    mentions = api.mentions_timeline()
+    return mentions
+
+def retweet(api):
+    mentions = get_mentions()
+    for mention in mentions:
+        try:
+            api.retweet(mention.id)
+            print('Retweeted: ' + mention.text)
+        except tweepy.TweepError as e:
+            print(e.reason)
+
+
+if __name__ == '__main__':
+    main()
