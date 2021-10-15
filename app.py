a11001d10cac399a7ecfee4776ba3b0cb92097ce
Lorenzo Battistela <lorenzowottrich@gmail.com>
2021-10-15 18:44:52 -0300

    fix repetead tweets error

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ import schedule
 import time
 import os
 
-
 consumer_key = os.getenv('CONSUMER_KEY')
 consumer_secret = os.getenv('CONSUMER_SECRET')
 acess_token = os.getenv('ACCESS_TOKEN')
 acess_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
+ret_mentions = []
+
 
 def main():
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -24,14 +25,33 @@ def get_mentions(api):
     mentions = api.mentions_timeline()
     return mentions
 
+
 def retweet(api):
     mentions = get_mentions(api)
+    rt = Retweets()
+    
     for mention in mentions:
+        retweeted_mentions = rt.get_retweeted_mentions()
         try:
-            api.retweet(mention.id)
-            print('Retweeted: ' + mention.text)
+            if mention.id in retweeted_mentions:
+                continue
+            else:
+                api.retweet(mention.id)
+                print('Retweeted: ' + mention.text)
+                rt.add_retweet(mention.id)
+
         except tweepy.TweepError as e:
             print(e.reason)
+
+class Retweets:
+    def __init__(self, retweeted_mentions=ret_mentions):
+        self.retweeted_mentions = ret_mentions
+    
+    def add_retweet(self, mention):
+        self.retweeted_mentions.append(mention)
+
+    def get_retweeted_mentions(self):
+        return self.retweeted_mentions
 
 
 if __name__ == '__main__':
